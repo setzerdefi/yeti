8de5ddd2310c0e0af01ec97147b825f3428160d0
setzerdefi <setzerdefi@outlook.com>
2024-03-26 20:47:14 +0100

    symbol

--- src/Yeti.sol
+++ src/Yeti.sol
@@ -6,5 +6,5 @@ import {ERC20TaxRewards} from "./ERC20TaxRewards.sol";
 contract Yeti is ERC20TaxRewards {
     uint256 public constant INITIAL_TOTAL_SUPPLY = 100_000_000 ether;
 
-    constructor() ERC20TaxRewards("Yeti", "YTI", INITIAL_TOTAL_SUPPLY) {}
+    constructor() ERC20TaxRewards("Yeti", "YETI", INITIAL_TOTAL_SUPPLY) {}
 }
